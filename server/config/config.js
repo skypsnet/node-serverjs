41ba970653fea10f4ff39980ecd89458db8f8358
Ricardo <rbalamcm@gmail.com>
2021-02-01 23:07:29 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
      urlDB = 'mongodb://localhost:27017/cafe'
 }else{
-     urlDB = 'mongodb+srv://skypsnet:Skypsnet1@cluster0.ude4q.mongodb.net/cafe'
+     urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
