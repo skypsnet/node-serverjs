2c039d8dc9a3312cbe41e70292131d424d73206d
Ricardo <rbalamcm@gmail.com>
2021-02-01 22:45:21 -0600

    RestServer -usuario

--- server/server.js
+++ server/server.js
@@ -1,47 +1,22 @@
+require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
-require('./config/config');
 
   app.use(bodyParser.urlencoded({extended:false}));
   app.use(bodyParser.json());
 
-  app.get('/usuario', function (req, res) {
-    res.json('get Usuario')
-  })
+  app.use(require('./routes/usuario'));
 
-  app.post('/usuario', function (req, res) {
-    let body = req.body;
-     
-    if(body.nombre === undefined){
-       
-        res.status(400).json({
-           ok: false,
-           mensaje: 'El nombre es necesario perro'
-       });
+  mongoose.connect(process.env.URLDB, {useNewUrlParser: true, useCreateIndex: true} ,(err,res)=>{
+        
+  if(err) throw new err;
 
-    } else {
-       
-        res.json({
-        persona: body
-        })
-    }
+  console.log('Base de datos Online')
 
-    
   });
-
-  app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-  })
-
-  app.delete('/usuario', function (req, res) {
-    res.json('delate Usuario')
-  })
-
    
   app.listen(process.env.PORT, ()=>{
       console.log('Escuchando puerto 3000')
